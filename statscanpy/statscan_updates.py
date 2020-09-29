6bed9550c2a1697db609e3538654ce0683bf7f3c
Andy <andrew@andrewdyck.com>
2020-09-29 07:58:40 -0600

    Create statscan_updates.py

---
+++ statscanpy/statscan_updates.py
@@ -0,0 +1,7 @@
+import requests
+
+# Changed Cube List
+# https://www150.statcan.gc.ca/t1/wds/rest/getChangedCubeList/2017-12-07
+
+# Changed Series List
+# https://www150.statcan.gc.ca/t1/wds/rest/getChangedSeriesList
